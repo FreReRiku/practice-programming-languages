798104375a8e61d910be9669cbe881b92482096d
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-02-09 20:15:42 +0900

    solve chapter3

--- kuru_c/chapter3/main.c
+++ kuru_c/chapter3/main.c
@@ -4,7 +4,17 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("HelloWorld");
-    return 0;
+    printf("HelloWorld\n");
+
+    /* \nを用いると改行することができる。
+    次のように文中に\nを入れても構わない。*/
+
+    printf("Hello\nworld\n");
+
+    /* \tは頭揃えのためにタブを挿入するために使う。*/
+    printf("Windows\tMicrosoft\n");
+    printf("macOS\tApple\n");
+
+
 }
 
