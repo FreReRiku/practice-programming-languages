693b6a1a2950a69537091304ec52c1abdad0a728
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-02-09 21:21:54 +0900

    solving chapter4

---
+++ kuru_c/chapter4/practice2.c
@@ -0,0 +1,11 @@
+/* 数値と文字列を組み合わせて表示する。 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d", 100);
+    printf("円\n");
+    return 0;
+}
+
---
+++ kuru_c/chapter4/practice3.c
@@ -0,0 +1,10 @@
+/* 1行で表すプログラム */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d円\n", 100);
+    return 0;
+}
+
