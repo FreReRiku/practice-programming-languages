824895b87a70c6a163854ce595c5d188283dbccb
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-02-21 14:36:47 +0900

    do chapter4

---
+++ kuru_c/chapter4/practice4.c
@@ -0,0 +1,12 @@
+// 複数の数値の表示
+// 簡単な数式を表示するプログラム
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d+", 100);
+    printf("%d=", 200);
+    printf("%d\n", 300);
+    return 0;
+}
+
---
+++ kuru_c/chapter4/practice6.c
@@ -0,0 +1,12 @@
+/*
+ * このプログラムではコンピューターに計算をさせる。
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 100 + 200);
+    return 0;
+}
+
