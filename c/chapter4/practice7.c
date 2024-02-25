ab21f544a16415cb952920042a6543a93b7dae07
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-02-26 00:28:02 +0900

    ファイルの整理を行った

---
+++ c/chapter4/practice7.c
@@ -0,0 +1,16 @@
+/*
+ * このファイルでは四項演算子を用いて、一通り計算してみる。
+ * */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("%d\n", 10 + 3);
+    printf("%d\n", 10 - 3);
+    printf("%d\n", 10 * 3);
+    printf("%d\n", 10 / 3);
+    printf("%d\n", 10 % 3);
+    return 0;
+}
+// なお整数計算の結果は四捨五入ではなく切り捨てとなる。
+
--- kuru_c/old_1/ex1.cpp
+++
@@ -1,9 +0,0 @@
-//例題1.1 ex1.cpp
-//画面に文字列を出力する。出力関数による。
-#include <stdio.h>
-
-int main(void)
-{
-    printf("C_language\n");
-    return 0;
-}
