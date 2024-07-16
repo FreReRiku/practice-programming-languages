ee5dfd399551f6a155f94ec523eb943b073b6bdf
KASAKURA Riku <kuranojou.big7@icloud.com>
2024-07-16 18:19:58 +0900

    knock11.cを作成しましたわ！

---
+++ c/hundred_knocks/Level1_Basic/knock11.c
@@ -0,0 +1,14 @@
+/* knock11.c */
+/* Hello, World! を10回繰り返すプログラム */
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 10; i++) {
+        printf("Hello, World!\n");
+    }
+
+    return 0;
+}
+
